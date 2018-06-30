aef1c828592793adefef3f00c21dc6444620bea1
William Zimmermann <williamzimmermann@MacBook-Pro-de-William-2.local>
2018-06-29 23:05:34 -0300

    Criamos uma view para fazer os acertos dos testes unitários.

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
+from django.http import HttpRequest
 from django.urls import resolve
 from django.test import TestCase
+
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -7,3 +9,12 @@ class HomePageTest(TestCase):
     def test_url_raiz_direciona_para_view_da_home_page(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_retorna_html_corretamente(self):
+        request = HttpRequest()
+        response = home_page(request)
+
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>Lista de Tarefas do Will</title>', html)
+        self.assertTrue(html.endswith('</html>'))
